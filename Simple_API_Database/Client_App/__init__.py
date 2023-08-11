b0177c0b53c394ce4ef1c7e0596da70e5e08b132
Caio Silva <35344295+Suundumused@users.noreply.github.com>
2023-08-11 17:31:11 -0300

    Update __init__.py

--- Simple_API_Database/Client_App/__init__.py
+++ Simple_API_Database/Client_App/__init__.py
@@ -11,7 +11,7 @@ import contextlib
 import keyboard
 
 class program:
-    def read_json_file(file):
+    def read_json_file(file): #abrir arquivo json contendo a requisição.
         print(file)
         filename = os.path.join(os.path.dirname(os.path.abspath(__name__)), "requests") + "/" + file
         print(filename)
@@ -19,18 +19,18 @@ class program:
         try:
             if not os.path.exists(filename):
                 os.system('cls')
-                print("No such file or directory.")
+                print("No such file or directory.") #erro se o arquivo nao existir.
             
             else:
                 with contextlib.ExitStack() as stack:
                     file = stack.enter_context(io.open(filename, mode='r', encoding='utf-8', errors='ignore'))
                     data = json.load(file)
                     
-                    if data['method'] == "GET":
+                    if data['method'] == "GET": #MÉTODOS SÃO SELECIONADOS A PARTIR DO ARQUIVO JSON NA PASTA REQUESTS.
                         response = requests.get(data['IP'], json=data)
                         
                         os.system('cls')
-                        print("\nResponse:", response.json())
+                        print("\nResponse:", response.json()) #RETORNA EM PRINT A RESPOSTA DO SERVIDOR.
                     
                     elif data['method'] == "PUT":
                         response = requests.put(data['IP'], json=data)
@@ -50,7 +50,7 @@ class program:
                         os.system('cls')
                         print("\nResponse:", response.json())
                     
-                    else:
+                    else: #ERRO SE NENHUM DOS 4 MÉTODOS FOREM USADOS.
                         os.system('cls')
                         print("\nUnknown method.")
             
@@ -60,24 +60,24 @@ class program:
             
         
         finally:
-            App = program()
+            App = program() #REINICIA TODO O APP QUANDO TUDO TERMINAR.
     
             Listener = threading.Thread(target=Interact)
             Listener.start()
             
     
     def __init__(self):
-        print("\n=================== | API_Client_V1 | ===================\n")
+        print("\n=================== | API_Client_V1 | ===================\n") #TELA DE BOAS VINDAS.
         print("Choose an option: \n")
         print("1 - Run through the request json file: 'Type filename'.")
         print("2 - Exit\n")
             
 class Interact:
-    def read_key():
+    def read_key(): #CAPTURA O QUE O USUÁRIO DIGITA NO TECLADO.
         event = keyboard.read_event()
         return event.name
     
-    def __init__(self):
+    def __init__(self): #INICIA LOOP: AGUARDANDO INTERAÇÃO DO USUÁRIO.
         while True:
             self.key = Interact.read_key()
                     
@@ -87,7 +87,7 @@ class Interact:
                     
                     break
         
-                elif self.key == '1':
+                elif self.key == '1': #OPÇÃO QUE INICIA INTERAÇÃO COM O SERVIDOR HTTP.
                     program.read_json_file(input("Type the filename including the extension: "))
                     
                     break
@@ -106,5 +106,5 @@ if __name__ == "__main__":
     
     App = program()
     
-    Listener = threading.Thread(target=Interact)
+    Listener = threading.Thread(target=Interact) #instancia processo de captura de chaves.
     Listener.start()
